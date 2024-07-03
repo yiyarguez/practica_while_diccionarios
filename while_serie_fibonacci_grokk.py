5e373b13023573c165a0b670aef807aabadba09e
Kary Rodríguez <yiyarguez@gmail.com>
2024-07-02 21:33:34 -0600

    Ejercicio serie fibonaccio resuelto

--- while_serie_fibonacci_grokk.py
+++ while_serie_fibonacci_grokk.py
@@ -11,6 +11,9 @@ En cada iteración, muestra el valor de a.
 Actualiza los valores de a y b de tal manera que a tome el valor de b y b tome el valor de a + b.
 Muestra los valores generados.
 
+Leonardo De Pisa (Fibonacci) -> 1,1,2,3,5,8,13,21,34,55,89,144,233,377      
+Nota: Verificar si la serie de fibonaccio inicia con 0, para este ejemplo tomaremos que si inicia con 0
+
 Juego de Pruebas:
 Entrada: n = 5
 Salida esperada: 0, 1, 1, 2, 3
@@ -18,5 +21,33 @@ Entrada: n = 8
 Salida esperada: 0, 1, 1, 2, 3, 5, 8, 13
 Entrada: n = 1
 Salida esperada: 0
+
+"""
+def fibonacci():
+    numero = int(input("Ingresa un numero: "))
+    a = 0
+    b = 1
+    
+    print(a)
+    while numero > 1: 
+        temp_a = a
+        a = b 
+        b = temp_a + b 
+        print(a)
+        
+        numero -= 1
+
+fibonacci()
+
+
+
+"""
+numero = 5
+
+numero    temp_a        a         b        print(a)    
+   5         0          1         1            1
+   4         1          1         2            1
+   3         1          2         3            2
+   2         2          3         5            3
 
 """
